b27e34fbd28e05478f31dbb73a0af30f0ab4f6ab
Priscilla Carneiro <57672050+Prica30@users.noreply.github.com>
2021-05-05 22:30:48 -0300

    Programa01
    Programa para ler dois valores inteiros, e depois mostrar na tela a soma desses números com uma mensagem explicativa.

---
+++ Programa01.java
@@ -0,0 +1,22 @@
+package curso_programacao;
+
+import java.util.Scanner;
+
+public class Programa01 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		int soma = a + b;
+		
+		System.out.println("SOMA = " + soma);
+		
+		sc.close();
+		
+	}
+
+}
